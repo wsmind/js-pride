21ffcf4e7057c87b550572090b21ecaa82da4bf3
Remi Papillie <remi.papillie@gmail.com>
2013-03-30 10:17:17 +0100

    better neons

--- shaders/neon.glsl
+++ shaders/neon.glsl
@@ -12,23 +12,28 @@ uniform mat4 viewProjectionMatrix;
 uniform vec3 sunDirection;
 
 varying vec3 fragNormal;
+varying vec3 fragTangent;
 
 //! VERTEX
 
 attribute vec3 position;
-attribute vec3 normal;
+//attribute vec3 normal;
+attribute vec3 tangent;
 
 void main(void)
 {
-	vec3 worldPosition = position + normal * 1.2;
+	vec3 cameraZ = (viewMatrix * vec4(0.0, 0.0, -1.0, 0.0)).xyz;
+	vec3 normal = cross(cameraZ, tangent);
+	vec3 worldPosition = position + normal * exp(-mod(time, 1.0)) * 0.4;
 	gl_Position = viewProjectionMatrix * vec4(worldPosition, 1.0);
 	fragNormal = normal;
+	fragTangent = tangent * 0.5 + 0.5;
 }
 
 //! FRAGMENT
 
 void main()
 {
-	vec3 color = vec3(0.0, pow(1.0 - abs(fragNormal.y), 2.0) * 0.8 * sin(time * time), 0.0);
+	vec3 color = vec3(0.0, pow(1.0 - abs(fragNormal.y), 4.0) * 0.8 /** sin(time * time)*/, 0.0);
 	gl_FragColor = vec4(color, 1.0);
 }
